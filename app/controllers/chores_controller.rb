29ac350c8dddcf624bd2d51d6fe788bdcd8fb60a
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-08-05 00:14:07 -0400

    I am breaking all of the things

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,4 +1,44 @@
 class ChoresController < ApplicationController
-  def new
+
+def index
+  @chores = Chore.all
+end
+
+def new
+  @occupant = Occupant.find(params[:occupant_id])
+  @chore = Chore.new
+end
+
+
+def create
+  @occupant = Occupant.find(params[:occupant_id])
+  @chore = Chore.create!(chore_params.merge(occupant: @occupant))
+  redirect_to occupant_chore_path(@occupant, @chore)
+end
+
+def show
+  @chore = Chore.find(params[:id])
+end
+
+def edit
+  @chore = Chore.find(params[:id])
+end
+
+def update
+  @chore = Chore.find(params[:id])
+  @occupant = Occupant.find(params[:occupant_id])
+  @chore.update(chore_params.merge(occupant: @occupant))
+  redirect_to occupant_chore_path(@chore.occupant, @chore)
+end
+
+def destroy
+  @chore = Chore.find(params[:id])
+  @chore.destroy
+  redirect_to chores_path
+end
+
+private
+  def chore_params
+    params.require(:chore).permit(:task, :description, :occupant_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 root to: 'occupants#index'
 
-resources :occupants
-resources :chores
+resources :occupants do
+  resources :chores
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
